8a291e2c046856091e85e92daf89bca6493c6f2e
Emily Laughead <elaugh2@illinois.edu>
2020-04-28 16:35:15 -0500

    actually have imgui up and running now

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,6 +1,8 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include "my_app.h"
+#include "CinderImGui.h"
+
 
 #include <cinder/app/App.h>
 
@@ -9,9 +11,17 @@ namespace myapp {
 
 using cinder::app::KeyEvent;
 
-MyApp::MyApp() { }
 
-void MyApp::setup() { }
+
+MyApp::MyApp() {
+
+}
+
+void MyApp::setup() {
+  ImGui::initialize();
+  ImGui::Text("Hello, world %d", 123);
+
+}
 
 void MyApp::update() { }
 
